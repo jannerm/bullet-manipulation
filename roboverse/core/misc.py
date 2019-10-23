821e4cb49a69ea69ac16cf5cd4a45593fe25ce4a
Jonathan Yang <jydumplings@gmail.com>
2019-10-23 12:54:20 -0700

    change random loading separation

--- roboverse/core/misc.py
+++ roboverse/core/misc.py
@@ -254,7 +254,7 @@ def load_random_objects(filePath, number):
         objectName = path[-1]
         obj = load_obj(filePath+'/ShapeNetCore_vhacd/{0}/{1}/model.obj'.format(dirName, objectName),
             filePath+'/ShapeNetCore.v2/{0}/{1}/models/model_normalized.obj'.format(dirName, objectName),
-            [positions[count][0], positions[count][1], 0], [0, 0, 1, 0], scale=random.uniform(0.5, 1) * scaling['{0}/{1}'.format(dirName, objectName)])
+            [positions[count][0], positions[count][1], 0.1], [0, 0, 1, 0], scale=random.uniform(0.5, 1) * scaling['{0}/{1}'.format(dirName, objectName)])
         object_ids.append(obj)
         count += 1
     return object_ids
