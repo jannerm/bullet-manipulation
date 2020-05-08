0e231889dffe48b55facbe6c63772c50f9d1222d
Jonathan Yang <jy2370@berkeley.edu>
2020-05-07 17:14:27 -0700

    Add shapenet utils files

--- roboverse/envs/widow200_box_packing_v2.py
+++ roboverse/envs/widow200_box_packing_v2.py
@@ -24,7 +24,8 @@ class WidowBoxPackingV2Env(Widow200GraspV5Env):
         return door_angle
 
     def get_info(self):
-        object_pos = self.get_object_midpoint('lego')
+        pass
+        #object_pos = self.get_object_midpoint('lego')
         object_goal_distance = np.linalg.norm(object_pos - self._goal_pos)
         end_effector_pos = self.get_end_effector_pos()
         object_gripper_distance = np.linalg.norm(object_pos - end_effector_pos)
@@ -41,26 +42,7 @@ class WidowBoxPackingV2Env(Widow200GraspV5Env):
         return info
 
     def get_reward(self, info):
-        lego_box_dist = np.linalg.norm(self.get_object_midpoint("lego") - self.get_object_midpoint("box"))
-        door_angle = self.get_door_angle()
-        ee_pos = np.array(self.get_end_effector_pos())
-        handle_pos = self.get_handle_pos()
-        gripper_handle_dist = np.clip(np.linalg.norm(ee_pos - handle_pos), 0, 1)
-        if self._reward_type == 'sparse':
-            # reward = int(door_angle > self.OPENED_DOOR_ANGLE)
-            reward = (
-                0.5*int(gripper_handle_dist < self.SUCCESS_THRESH) +
-                0.5*int(door_angle > self.OPENED_DOOR_ANGLE)
-            )
-        elif self._reward_type == 'shaped':
-            # door_angle_reward = np.clip(door_angle, 0, self.OPENED_DOOR_ANGLE) / self.OPENED_DOOR_ANGLE
-            door_angle_reward = door_angle
-            # reward = np.clip(door_angle_reward - gripper_handle_dist, 0, 1)
-            reward = door_angle_reward - (5 * gripper_handle_dist)
-        else:
-            raise NotImplementedError
-
-        return reward
+        return 0
 
 
 if __name__ == "__main__":
