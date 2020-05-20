5055f8f3b07f4c4ee727398deb9683cbb8f3a03d
Soroush Nasiriany <snasiriany@gmail.com>
2020-05-20 15:46:48 -0700

    added keyboard control

---
+++ sandbox/keyboard_control.py
@@ -0,0 +1,99 @@
+"""
+Use this script to control the env with your keyboard.
+For this script to work, you need to have the PyGame window in focus.
+
+See/modify `char_to_action` to set the key-to-action mapping.
+"""
+import sys
+
+import numpy as np
+
+import roboverse as rv
+
+import pygame
+from pygame.locals import QUIT, KEYDOWN
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+
+
+char_to_action = {
+    'w': np.array([0, 0, 1, 0]),
+    'a': np.array([0, -1, 0, 0]),
+    's': np.array([0, 0, -1, 0]),
+    'd': np.array([0, 1, 0, 0]),
+    # 'q': np.array([1, -1, 0, 0]),
+    # 'e': np.array([-1, -1, 0, 0]),
+    # 'z': np.array([1, 1, 0, 0]),
+    # 'c': np.array([-1, 1, 0, 0]),
+    # 'k': np.array([0, 0, 1, 0]),
+    # 'j': np.array([0, 0, -1, 0]),
+    'h': 'close',
+    'l': 'open',
+    'x': 'toggle',
+    'r': 'reset',
+    'p': 'put obj in hand',
+    'g': 'goal',
+}
+
+env_id = 'SawyerBase2d-v0'
+# env_id = 'SawyerGraspOne2d-v0'
+# env_id = 'SawyerLid-v0'
+# env_id = 'SawyerSoup2d-v0'
+# env_id = 'SawyerMultiSoup2d-v0'
+
+env = rv.make(
+    env_id,
+    action_scale=.05,
+    action_repeat=50,
+    timestep=1. / 120,
+
+    solver_iterations=250,
+
+    max_force=100,
+
+    gui=True
+)
+print(env.observation_space, env.action_space)
+
+NDIM = env.action_space.low.size
+lock_action = False
+obs = env.reset()
+action = np.zeros(10)
+while True:
+    done = False
+    if not lock_action:
+        action[:3] = 0
+    for event in pygame.event.get():
+        event_happened = True
+        if event.type == QUIT:
+            sys.exit()
+        if event.type == KEYDOWN:
+            char = event.dict['key']
+            new_action = char_to_action.get(chr(char), None)
+            # print(new_action)
+            if new_action == 'toggle':
+                lock_action = not lock_action
+            elif new_action == 'reset':
+                done = True
+            elif new_action == 'goal':
+                ob = env.reset()
+                env.set_to_goal({"state_desired_goal": ob["state_desired_goal"]})
+            elif new_action == 'close':
+                action[3] = 1
+            elif new_action == 'open':
+                action[3] = -1
+            elif new_action == 'put obj in hand':
+                print("putting obj in hand")
+                env.put_obj_in_hand()
+                action[3] = 1
+            elif new_action is not None:
+                action[:3] = new_action[:3]
+            else:
+                action = np.zeros(10)
+            if action.any():
+                print(action[:4])
+                env.step(action[:4])
+    if done:
+        obs = env.reset()
+    env.render()
