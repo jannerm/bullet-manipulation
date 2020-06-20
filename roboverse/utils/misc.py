5c04ac96ab321b8b4c3f507e95c0731bf279bc86
huihanl <liuhh@berkeley.edu>
2020-06-20 14:58:17 -0700

    delete merge conflicts

--- roboverse/utils/misc.py
+++ roboverse/utils/misc.py
@@ -34,74 +34,6 @@ def true_angle_diff(theta):
 
 class DemoPool:
 
-<<<<<<< HEAD
-	def __init__(self, max_size=1e6):
-		self._keys = ('observations', 'actions', 'next_observations', 'rewards', 'terminals')
-		self._fields = {}
-		self._max_size = int(max_size)
-		self._size = 0
-		self._pointer = 0
-
-	@property
-	def size(self):
-		return self._size
-
-	def add_sample(self, *arrays):
-		if self._size:
-			self._add(arrays)
-		else:
-			self._init(arrays)
-
-		self._advance()
-		# print(self._size, self._pointer)
-
-	def save(self, params, *savepath):
-		savepath = os.path.join(*savepath)
-		self._prune()
-		save_info = [(key, self._fields[key].shape) for key in self._keys]
-		print('[ DemoPool ] Saving to: {} | {}'.format(savepath, save_info))
-		pickle.dump(self._fields, open(savepath, 'wb+'))
-
-		## save params
-		params_path = savepath.replace('pool', 'params')
-		pickle.dump(params, open(params_path, 'wb+'))
-
-	def _add(self, arrays):
-		for key, array in zip(self._keys, arrays):
-			self._fields[key][self._pointer] = array
-
-	def _init(self, arrays):
-		for key, array in zip(self._keys, arrays):
-			shape = array.shape if type(array) == np.ndarray else (1,)
-			dtype = array.dtype if type(array) == np.ndarray else type(array)
-			self._fields[key] = np.zeros((self._max_size, *shape), dtype=dtype)
-			self._fields[key][self._pointer] = array
-			# print(key, self._fields[key].shape, self._fields[key].dtype)
-
-	def _advance(self):
-		self._size = min(self._size + 1, self._max_size)
-		self._pointer = (self._pointer + 1) % self._max_size
-
-	def _prune(self):
-		for key in self._keys:
-			self._fields[key] = self._fields[key][:self._size]
-
-	def reduce_size(self, desired_size):
-		self._size = desired_size
-		self._pointer = desired_size
-		for key in self._keys:
-			self._fields[key] = self._fields[key][:desired_size]
-
-	def merge(self, other_pool):
-		fields = other_pool.get_samples()
-		for i in range(other_pool.size()):
-			arrays = [fields[key][i] for key in self._keys]
-			self.add_sample(arrays[0], arrays[1], arrays[2], arrays[3], arrays[4])
-
-	def get_samples(self):
-		self._prune()
-		return self._fields
-=======
     def __init__(self, max_size=1e6):
         self._keys = ('observations', 'actions', 'next_observations', 'rewards', 'terminals')
         self._fields = {}
@@ -156,8 +88,6 @@ class DemoPool:
     def get_samples(self):
         self._prune()
         return self._fields
-
->>>>>>> bedeeca2d0dcd26c2f382f8e4097569878edb091
 
 class Meta:
 
