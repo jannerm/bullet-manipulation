c3e0cf4e1f39b7fe9944e79d72fec781f34629c7
Albert Yu <albertyu@berkeley.edu>
2020-06-21 01:17:07 -0700

    Add add_object_textures.py script to automate adding textures to objects.

--- sandbox/debug_object.py
+++ sandbox/debug_object.py
@@ -5,6 +5,10 @@ import pdb
 import roboverse.bullet as bullet
 import roboverse.devices as devices
 
+from roboverse.utils.shapenet_utils import load_shapenet_object, \
+    import_shapenet_metadata
+obj_path_map, path_scaling_map = import_shapenet_metadata()
+
 # space_mouse = devices.SpaceMouse()
 # space_mouse.start_control()
 
@@ -14,8 +18,18 @@ bullet.setup()
 ## load meshes
 table = bullet.objects.table()
 # spam = bullet.objects.spam()
-box = bullet.objects.box()
-box_open_top = bullet.objects.box_open_top()
+# box = bullet.objects.box()
+# box_open_top = bullet.objects.box_open_top()
+
+object_names = ["stalagcite_chunk"]
+
+object_path_dict = dict(
+    [(obj, path) for obj, path in obj_path_map.items() if obj in object_names])
+scaling = dict(
+    [(path, path_scaling_map[path]) for _, path in object_path_dict.items()])
+
+for object_name in object_names:
+	load_shapenet_object(obj_path_map[object_name], scaling, np.array([.8, 0, -.2]), scale_local=0.5)
 
 while True:
     time.sleep(0.01)
