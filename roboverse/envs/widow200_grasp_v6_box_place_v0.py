972cfb560e8336f5b5558558815856b520a4eed8
Albert Yu <albertyu@berkeley.edu>
2020-06-22 19:20:51 -0700

    Added parent class RandObjEnv so that all the train/test/random object logic is handled by one class.

--- roboverse/envs/widow200_grasp_v6_box_place_v0.py
+++ roboverse/envs/widow200_grasp_v6_box_place_v0.py
@@ -1,4 +1,5 @@
 from roboverse.envs.widow200_grasp_v5_and_place_v0 import Widow200GraspV5AndPlaceV0Env
+from roboverse.envs.rand_obj import RandObjEnv
 from roboverse.envs.env_object_list import (
     POSSIBLE_TRAIN_OBJECTS, POSSIBLE_TRAIN_SCALINGS,
     POSSIBLE_TEST_OBJECTS, POSSIBLE_TEST_SCALINGS)
@@ -83,64 +84,11 @@ class Widow200GraspV6BoxPlaceV0Env(Widow200GraspV5AndPlaceV0Env):
             object_gripper_success=object_gripper_success)
         return info
 
-class Widow200GraspV6BoxPlaceV0RandObjEnv(Widow200GraspV6BoxPlaceV0Env):
+class Widow200GraspV6BoxPlaceV0RandObjEnv(RandObjEnv, Widow200GraspV6BoxPlaceV0Env):
     """
     Generalization env. Randomly samples one of the following objects
-    every time the env resets.
+    every time the env resets for the V6BoxPlace task.
     """
-    def __init__(self,
-                 *args,
-                 in_eval=False,
-                 success_dist_threshold=0.04,
-                 train_scaling_list=[0.3]*10,
-                 test_scaling_list=[0.3]*10,
-                 possible_train_objects="default",
-                 possible_test_objects="default",
-                 **kwargs):
-        self.in_eval = in_eval # True when doing evaluation
-        # so that we use novel test_objects.
-
-        if possible_train_objects == "default":
-            # Decided based on object_success_best_scaling.csv
-            self.possible_train_objects = POSSIBLE_TRAIN_OBJECTS[:10]
-            self.possible_train_scaling_local_list = POSSIBLE_TRAIN_SCALINGS[:10]
-        else:
-            assert isinstance(possible_train_objects, list)
-            self.possible_train_objects = possible_train_objects
-            self.possible_train_scaling_local_list = train_scaling_list
-
-        if possible_test_objects == "default":
-            self.possible_test_objects = POSSIBLE_TEST_OBJECTS[:10]
-            self.possible_test_scaling_local_list = POSSIBLE_TEST_SCALINGS[:10]
-        else:
-            assert isinstance(possible_test_objects, list)
-            self.possible_test_objects = possible_test_objects
-            self.possible_test_scaling_local_list = test_scaling_list
-
-        if self.in_eval:
-            self.possible_objects = self.possible_test_objects
-            self.possible_scaling_local_list = \
-                self.possible_test_scaling_local_list
-        else:
-            self.possible_objects = self.possible_train_objects
-            self.possible_scaling_local_list = \
-                self.possible_train_scaling_local_list
-
-        self.success_dist_threshold = success_dist_threshold
-        super().__init__(*args,
-            object_names=self.possible_objects,
-            success_dist_threshold=self.success_dist_threshold,
-            scaling_local_list=self.possible_scaling_local_list,
-            **kwargs)
-
-    def reset(self):
-        """Currently only implemented for selecting 1 object at random"""
-        chosen_obj_idx = np.random.randint(0, len(self.possible_objects))
-        self.object_names = [self.possible_objects[chosen_obj_idx]]
-        self.scaling_local_list = [
-            self.possible_scaling_local_list[chosen_obj_idx]]
-        # print("self.object_names", self.object_names, self.scaling_local_list)
-        return super().reset()
 
 
 if __name__ == "__main__":
--- roboverse/envs/widow200_grasp_v6_box_v0.py
+++ roboverse/envs/widow200_grasp_v6_box_v0.py
@@ -1,4 +1,5 @@
 from roboverse.envs.widow200_grasp_v6 import Widow200GraspV6Env
+from roboverse.envs.rand_obj import RandObjEnv
 from roboverse.envs.env_object_list import (
     POSSIBLE_TRAIN_OBJECTS, POSSIBLE_TRAIN_SCALINGS,
     POSSIBLE_TEST_OBJECTS, POSSIBLE_TEST_SCALINGS)
@@ -34,64 +35,11 @@ class Widow200GraspV6BoxV0Env(Widow200GraspV6Env):
         super()._load_meshes()
         self._box = bullet.objects.box_open_top()
 
-class Widow200GraspV6BoxV0RandObjEnv(Widow200GraspV6BoxV0Env):
+class Widow200GraspV6BoxV0RandObjEnv(RandObjEnv, Widow200GraspV6BoxV0Env):
     """
     Generalization env. Randomly samples one of the following objects
-    every time the env resets.
+    every time the env resets for the V6Box task.
     """
-    def __init__(self,
-                 *args,
-                 in_eval=False,
-                 success_dist_threshold=0.04,
-                 train_scaling_list=[0.3]*10,
-                 test_scaling_list=[0.3]*10,
-                 possible_train_objects="default",
-                 possible_test_objects="default",
-                 **kwargs):
-        self.in_eval = in_eval # True when doing evaluation
-        # so that we use novel test_objects.
-
-        if possible_train_objects == "default":
-            # Decided based on object_success_best_scaling.csv
-            self.possible_train_objects = POSSIBLE_TRAIN_OBJECTS[:10]
-            self.possible_train_scaling_local_list = POSSIBLE_TRAIN_SCALINGS[:10]
-        else:
-            assert isinstance(possible_train_objects, list)
-            self.possible_train_objects = possible_train_objects
-            self.possible_train_scaling_local_list = train_scaling_list
-
-        if possible_test_objects == "default":
-            self.possible_test_objects = POSSIBLE_TEST_OBJECTS[:10]
-            self.possible_test_scaling_local_list = POSSIBLE_TEST_SCALINGS[:10]
-        else:
-            assert isinstance(possible_test_objects, list)
-            self.possible_test_objects = possible_test_objects
-            self.possible_test_scaling_local_list = test_scaling_list
-
-        if self.in_eval:
-            self.possible_objects = self.possible_test_objects
-            self.possible_scaling_local_list = \
-                self.possible_test_scaling_local_list
-        else:
-            self.possible_objects = self.possible_train_objects
-            self.possible_scaling_local_list = \
-                self.possible_train_scaling_local_list
-
-        self.success_dist_threshold = success_dist_threshold
-        super().__init__(*args,
-            object_names=self.possible_objects,
-            success_dist_threshold=self.success_dist_threshold,
-            scaling_local_list=self.possible_scaling_local_list,
-            **kwargs)
-
-    def reset(self):
-        """Currently only implemented for selecting 1 object at random"""
-        chosen_obj_idx = np.random.randint(0, len(self.possible_objects))
-        self.object_names = [self.possible_objects[chosen_obj_idx]]
-        self.scaling_local_list = [
-            self.possible_scaling_local_list[chosen_obj_idx]]
-        # print("self.object_names", self.object_names, self.scaling_local_list)
-        return super().reset()
 
 
 if __name__ == "__main__":
