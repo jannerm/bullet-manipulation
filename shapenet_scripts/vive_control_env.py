427fe80bdd65b389f59a5e407f98a54d393db2d8
huihanl <liuhh@berkeley.edu>
2020-07-03 17:16:34 -0700

    vr data collection script

--- shapenet_scripts/vive_control_env.py
+++ shapenet_scripts/vive_control_env.py
@@ -12,6 +12,7 @@ from PIL import Image
 import argparse
 import time
 
+print(p.SHARED_MEMORY)
 # connect to the VR server (by p.SHARED_MEMORY)
 bullet.connect()
 bullet.setup()
@@ -35,7 +36,10 @@ num_grasps = 0
 ORIENTATION_ENABLED = True
 data = []
 
+min_traj_length = 22
+
 def collect_one_trajectory(env, env2, pool, render_images):
+    global min_traj_length
 
     # get VR controller output at one timestamp
     def get_VR_output():
@@ -63,7 +67,7 @@ def collect_one_trajectory(env, env2, pool, render_images):
         cont_orient = bullet.deg_to_quat([180, 0, 0])
         if ORIENTATION_ENABLED:
             cont_orient = e[ORIENTATION]
-            cont_orient = list(cont_orient)
+            cont_orient = bullet.quat_to_deg(list(cont_orient))
 
 
         action = [cont_pos[0] - ee_pos[0], cont_pos[1] - ee_pos[1], cont_pos[2] - ee_pos[2]]
@@ -72,13 +76,18 @@ def collect_one_trajectory(env, env2, pool, render_images):
         #prev_pos = list(cont_pos)
 
         grip = trigger
-        action = np.append(action, [grip])
+
+        ee_theta = bullet.get_link_state(
+            env._sawyer, env._end_effector, 'theta', quat_to_deg=True)
+
+        action = np.append(action, (cont_orient[2] - ee_theta[2]) / 20)
 
         #action = np.append(action, cont_orient)
         return action
 
     o = env.reset()
-    time.sleep(1)
+
+    time.sleep(1.5)
     images = []
 
     accept = False
@@ -97,7 +106,7 @@ def collect_one_trajectory(env, env2, pool, render_images):
     #prev_pos = e[POSITION]
     #prev_pos = list(prev_pos)
     # Collect a fixed length of trajectory
-    for i in range(50):
+    for i in range(args.num_timesteps):
 
         # for _ in range(3):
         #     action += get_VR_output()
@@ -107,28 +116,37 @@ def collect_one_trajectory(env, env2, pool, render_images):
         action = get_VR_output()
         action *= 1.5
 
-        if False:
-            img = env.render()
+        if True:
+            img = env.render_obs()
             images.append(Image.fromarray(np.uint8(img)))
 
         observation = env.get_observation()
-
+        #print(env.render_obs())
         traj["observations"].append(observation)
         next_state, reward, done, info = env.step(action)
         #print("ENV: observation of object position: ", next_state[-7:-4])
-
+        #print(next_state["image"])
         traj["next_observations"].append(next_state)
         traj["actions"].append(action)
         traj["rewards"].append(reward)
         traj["terminals"].append(done)
         traj["agent_infos"].append(info)
         traj["env_infos"].append(info)
-        pool.add_sample(observation, action, next_state, reward, done)
+
+        #pool.add_sample(observation, action, next_state, reward, done)
         time.sleep(0.03)
 
+        if reward > 0:
+            print("grasp success")
+            accept = "y" #input("Accept trajectory? [y/n]\n")
+            traj_len = len(traj["next_observations"])
+            if traj_len < min_traj_length:
+                min_traj_length = traj_len
+
+            return accept, images, traj
         #if info["object_goal_distance_z"] < 0.01:
-    accept = "y" #input("Accept trajectory? [y/n]\n")
-    return accept, images, traj
+
+    return "n", images, traj
 
 
 def main(args):
@@ -144,12 +162,10 @@ def main(args):
         os.makedirs(video_save_path)
 
     reward_type = 'grasp_only'
-    env = roboverse.make('SawyerGraspOne-v0', reward_type=reward_type,
-                         gui=args.gui, randomize=True,#args.randomize,
-                         observation_mode=args.observation_mode)
-    #env2 = roboverse.make('SawyerGraspOne-v0', reward_type=reward_type,
-    #                 gui=args.gui, randomize=True,#args.randomize,
-    #                 observation_mode=args.observation_mode)
+
+    env = roboverse.make('SawyerGraspOneV2-v0', reward_type=reward_type, randomize=True, observation_mode='pixels_debug')
+    env.reset()
+
     num_grasps = 0
     pool = roboverse.utils.DemoPool()
     success_pool = roboverse.utils.DemoPool()
@@ -163,30 +179,28 @@ def main(args):
 
         while success != 'y' and success != 'Y':
             print("failed for trajectory {}, collect again".format(j))
-            success, images, traj = collect_one_trajectory(env, None, pool, render_images)            
+            success, images, traj = collect_one_trajectory(env, None, pool, render_images)          
         
-        #env2.reset()
-        #actions = traj["actions"]     
-        #for a in actions:
-        #    next_state2, reward, done, info = env2.step(a)
-        #    print("ENV2: observation of object position: ", next_state2[-7:-4])   
-
         data.append(traj)
-
-        if False:
+        if render_images:
             images[0].save('{}/{}.gif'.format(video_save_path, j),
                            format='GIF', append_images=images[1:],
                            save_all=True, duration=100, loop=0)
 
+        if j % 100 == 0:
+            path = os.path.join(__file__, "../..", "vr_demos_success_{}_checkpoint_{}.npy".format(timestamp, j))
+            np.save(path, data)
     path = os.path.join(__file__, "../..", "vr_demos_success_{}.npy".format(timestamp))
     np.save(path, data)
+
+    print("min_traj_length: ", min_traj_length)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data-save-directory", type=str, default="vr_expert_demos")
-    parser.add_argument("-n", "--num-trajectories", type=int, default=200)
+    parser.add_argument("-n", "--num-trajectories", type=int, default=5)
     parser.add_argument("-p", "--num-parallel-threads", type=int, default=1)
-    parser.add_argument("--num-timesteps", type=int, default=50)
+    parser.add_argument("--num-timesteps", type=int, default=22)
     parser.add_argument("--noise-std", type=float, default=0.1)
     parser.add_argument("--video_save_frequency", type=int,
                         default=1, help="Set to zero for no video saving")
