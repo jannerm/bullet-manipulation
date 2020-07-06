077984f292425061a795afd8cdda253a8b4e2fda
Avi Singh <avisingh599@gmail.com>
2020-07-05 22:51:37 -0700

    Add V7 grasping

---
+++ roboverse/envs/widow200_grasp_v7.py
@@ -0,0 +1,30 @@
+import numpy as np
+from roboverse.envs.widow200_grasp_v6 import Widow200GraspV6Env
+import roboverse.bullet as bullet
+
+REWARD_FAIL = 0.0
+REWARD_SUCCESS = 1.0
+
+
+class Widow200GraspV7Env(Widow200GraspV6Env):
+    def __init__(self,
+                 *args,
+                 **kwargs):
+        self.use_goal_location_for_reward = True
+        super().__init__(*args, **kwargs)
+
+    def get_reward(self, info):
+        object_list = self._objects.keys()
+        reward = REWARD_FAIL
+        end_effector_pos = np.asarray(self.get_end_effector_pos())
+        for object_name in object_list:
+            object_info = bullet.get_body_info(self._objects[object_name],
+                                               quat_to_deg=False)
+            object_pos = np.asarray(object_info['pos'])
+            object_gripper_distance = np.linalg.norm(
+                object_pos - end_effector_pos)
+            gripper_goal_distance= np.linalg.norm(
+                end_effector_pos - self.gripper_goal_location)
+            if object_gripper_distance < 0.07 and gripper_goal_distance < 0.03:
+                reward = REWARD_SUCCESS
+        return reward
--- shapenet_scripts/scripted_collect.py
+++ shapenet_scripts/scripted_collect.py
@@ -463,6 +463,90 @@ def scripted_grasping_V6(env, pool, success_pool, noise=0.2):
     if rewards[-1] > 0:
         success_pool.add_path(path)
 
+
+def scripted_grasping_V7(env, pool, success_pool, noise=0.2):
+    observation = env.reset()
+    object_ind = np.random.randint(0, env._num_objects)
+    margin = 0.025
+    actions, observations, next_observations, rewards, terminals, infos = \
+        [], [], [], [], [], []
+
+    dist_thresh = 0.045 + np.random.normal(scale=0.01)
+    dist_thresh = np.clip(dist_thresh, 0.035, 0.060)
+
+    for _ in range(args.num_timesteps):
+
+        if isinstance(observation, dict):
+            object_pos = observation[env.object_obs_key][
+                         object_ind * 7 : object_ind * 7 + 3]
+            ee_pos = observation[env.fc_input_key][:3]
+        else:
+            object_pos = observation[
+                         object_ind * 7 + 8: object_ind * 7 + 8 + 3]
+            ee_pos = observation[:3]
+
+        # object_lifted_with_margin = object_pos[2] > (env._reward_height_thresh + margin)
+
+        object_gripper_dist = np.linalg.norm(object_pos - ee_pos)
+        theta_action = 0.
+
+        if object_gripper_dist > dist_thresh and env._gripper_open:
+            # print('approaching')
+            action = (object_pos - ee_pos) * 7.0
+            xy_diff = np.linalg.norm(action[:2] / 7.0)
+            if xy_diff > 0.02:
+                action[2] = 0.0
+            action = np.concatenate(
+                (action, np.asarray([theta_action, 0., 0.])))
+        elif env._gripper_open:
+            # print('gripper closing')
+            action = (object_pos - ee_pos) * 7.0
+            action = np.concatenate(
+                (action, np.asarray([0., -0.7, 0.])))
+        else:
+            action = env.gripper_goal_location - env.get_end_effector_pos()
+            action = np.concatenate(
+                (action, np.asarray([0., 0., 0.])))
+
+        # action += np.random.normal(scale=noise, size=(6,))
+        action[:3] += np.random.normal(scale=noise, size=(3,))
+        action[3] += np.random.normal(scale=noise*0.1)
+        action[4:] += np.random.normal(scale=noise, size=(2,))
+        action = np.clip(action, -1 + EPSILON, 1 - EPSILON)
+
+        next_observation, reward, done, info = env.step(action)
+
+        actions.append(action)
+        observations.append(observation)
+        rewards.append(reward)
+        terminals.append(done)
+        infos.append(info)
+        next_observations.append(next_observation)
+
+        observation = next_observation
+
+        if done:
+            break
+
+    path = dict(
+        actions=actions,
+        rewards=np.asarray(rewards).reshape((-1, 1)),
+        terminals=np.asarray(terminals).reshape((-1, 1)),
+        infos=infos,
+        observations=observations,
+        next_observations=next_observations,
+    )
+
+    if not isinstance(observation, dict):
+        path_length = len(rewards)
+        path['agent_infos'] = np.asarray([{} for i in range(path_length)])
+        path['env_infos'] = np.asarray([{} for i in range(path_length)])
+
+    pool.add_path(path)
+    if rewards[-1] > 0:
+        success_pool.add_path(path)
+
+
 def scripted_grasping_V6_placing_V0(env, pool, success_pool, noise=0.2):
     observation = env.reset()
     object_ind = 0
@@ -687,6 +771,11 @@ def main(args):
             success = False
             scripted_grasping_V6_placing_V0(
                 env, railrl_pool, railrl_success_pool, noise=args.noise_std)
+        elif args.env in V7_GRASPING_ENVS:
+            assert not render_images
+            success = False
+            scripted_grasping_V7(env, railrl_pool, railrl_success_pool,
+                                 noise=args.noise_std)
         else:
             raise NotImplementedError
 
@@ -731,7 +820,7 @@ def main(args):
                         '{}_pool_{}_success_only.pkl'.format(
                             timestamp, pool_size))
         pickle.dump(railrl_success_pool, open(path, 'wb'), protocol=4)
-        if args.env in (V6_GRASPING_ENVS +
+        if args.env in (V6_GRASPING_ENVS + V7_GRASPING_ENVS +
             V6_GRASPING_V0_PLACING_ENVS + V6_GRASPING_V0_PLACING_ONLY_ENVS):
             # For non terminating envs: we reshape the rewards
             # array and count the number of trajectories with
@@ -778,7 +867,7 @@ if __name__ == "__main__":
     if args.env in V2_GRASPING_ENVS:
         args.num_timesteps = 20
         assert args.observation_mode != 'pixels'
-    elif args.env in V4_GRASPING_ENVS + V6_GRASPING_ENVS:
+    elif args.env in V4_GRASPING_ENVS + V6_GRASPING_ENVS + V7_GRASPING_ENVS:
         args.num_timesteps = 25
         assert args.observation_mode != 'pixels'
     elif args.env in V6_GRASPING_V0_PLACING_ENVS:
