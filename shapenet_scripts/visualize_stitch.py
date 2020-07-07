9929584c4b056f5297e38060824f427b49ca6564
Albert Yu <albertyu@berkeley.edu>
2020-07-07 09:55:04 -0700

    Debug visualize_stitch.py.

--- shapenet_scripts/visualize_stitch.py
+++ shapenet_scripts/visualize_stitch.py
@@ -9,9 +9,9 @@ import os.path as osp
 # FILE_PICK = '/media/avi/data/Work/github/jannerm/bullet-manipulation/data/july6_test_4_Widow200GraspV7BoxV0-v0_pixels_debug_50_sparse_reward_scripted_actions_fixed_position_noise_std_0.2/railrl_consolidated.pkl'
 # FILE_PLACE = '/media/avi/data/Work/github/jannerm/bullet-manipulation/data/july6_test_2_Widow200GraspV6BoxPlaceOnlyV0-v0_pixels_debug_50_sparse_reward_scripted_actions_fixed_position_noise_std_0.05/railrl_consolidated.pkl'
 # OUTPUT_DIR = '/media/avi/data/Work/data/stitch_debug_v7_part2/'
-FILE_PICK = '/media/avi/data/Work/github/jannerm/bullet-manipulation/data/july6_notheta_pt2_Widow200GraspV7BoxV0-v0_pixels_debug_100_sparse_reward_scripted_actions_fixed_position_noise_std_0.2/railrl_consolidated.pkl'
-FILE_PLACE = '/media/avi/data/Work/github/jannerm/bullet-manipulation/data/july6_notheta_pt2_Widow200GraspV6BoxPlaceOnlyV0-v0_pixels_debug_100_sparse_reward_scripted_actions_fixed_position_noise_std_0.2/railrl_consolidated.pkl'
-OUTPUT_DIR = '/media/avi/data/Work/data/stitch_debug_v7_notheta_pt2/'
+FILE_PICK = '/nfs/kun1/users/avi/batch_rl_datasets/july6_Widow200GraspV7BoxV0-v0_pixels_debug_40K_sparse_reward_scripted_actions_fixed_position_noise_std_0.2/railrl_consolidated.pkl'
+FILE_PLACE = '/nfs/kun1/users/avi/batch_rl_datasets/july6_Widow200GraspV6BoxPlaceOnlyV0-v0_pixels_debug_40K_sparse_reward_scripted_actions_fixed_position_noise_std_0.2/railrl_consolidated.pkl'
+OUTPUT_DIR = 'data'
 if not osp.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 def process_image(image):
@@ -48,7 +48,7 @@ with open(FILE_PLACE, 'rb') as f:
 # ax2.scatter(pick_final_states[:, 0], pick_final_states[:, 2], c='b', alpha=0.3)
 # ax3.scatter(place_starting_states[:, 1], place_starting_states[:, 2], c='g')
 # ax3.scatter(pick_final_states[:, 1], pick_final_states[:, 2], c='b', alpha=0.3)
-plt.show()
+# plt.show()
 ########
 #IMAGES#
 ########
@@ -56,6 +56,8 @@ points_to_plot = 100
 place_starting_img = []
 pick_final_img = []
 num_succ = 0
+print("pick._rewards.shape", pick._rewards.shape)
+print("place._rewards.shape", place._rewards.shape)
 for i in range(0, pick._rewards.shape[0], 5):
     if pick._rewards[i] > 0.0:
         # pick_final_states.append(pick._obs['robot_state'][i][:3])
