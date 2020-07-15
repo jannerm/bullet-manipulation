4946d43966abab23831843b614e273c3bc7887d8
Albert Yu <albertyu@berkeley.edu>
2020-07-15 04:22:23 -0700

    Make load_buffer_image_rollouts.py support passing in command line args.

--- shapenet_scripts/load_buffer_image_rollouts.py
+++ shapenet_scripts/load_buffer_image_rollouts.py
@@ -1,17 +1,15 @@
 from railrl.data_management.replay_buffer import ReplayBuffer
+from railrl.data_management.obs_dict_replay_buffer import ObsDictReplayBuffer
 import numpy as np
 import pickle
 from PIL import Image
 import skimage.io as skii
 import skimage.transform as skit
 import os
+import argparse
+import roboverse
 
 # buffer_path = "/home/albert/dev/bullet-manipulation-avi-master/data/data_Widow200GraspV6BoxPlaceV0-v0_pixels_debug_1_sparse_reward_scripted_actions_fixed_position_noise_std_0.1/2020-06-22T10-42-24/2020-06-22T10-42-24_pool_31.pkl"
-
-buffer_path = "/home/albert/batch_rl_datasets/test_jul11_v8_Widow200GraspV6DrawerOpenOnlyV0-v0_pixels_debug_20_sparse_reward_scripted_actions_fixed_position_noise_std_0.2/railrl_consolidated.pkl"
-
-traj_len = 30
-num_rollouts_to_save = 20
 
 def get_img_np_from_buffer(buffer_path, img_side):
     with open(buffer_path, 'rb') as f:
@@ -26,6 +24,8 @@ def get_img_np_from_buffer(buffer_path, img_side):
 def save_video_from_img_obs(img_obs, img_side, traj_len, num_rollouts_to_save):
     if not os.path.exists('data'):
         os.makedirs('data')
+    # Assumes all paths have length traj_len
+    num_rollouts_to_save = min(num_rollouts_to_save, img_obs.shape[0] // traj_len)
     for n in range(num_rollouts_to_save):
         images = []
         for i in range(traj_len):
@@ -36,6 +36,15 @@ def save_video_from_img_obs(img_obs, img_side, traj_len, num_rollouts_to_save):
             save_all=True, append_images=images[1:], duration=traj_len*2, loop=0)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--buffer_path", type=str, required=True)
+    parser.add_argument("-l", "--traj_len", type=int, required=True)
+    parser.add_argument("-n", "--num_rollouts_to_save", type=int, default=20)
+    args = parser.parse_args()
+
+    buffer_path = args.buffer_path
+    traj_len = args.traj_len
+    num_rollouts_to_save = args.num_rollouts_to_save
     img_side = 48
     img_obs = get_img_np_from_buffer(buffer_path, img_side)
     save_video_from_img_obs(img_obs, img_side, traj_len, num_rollouts_to_save)
