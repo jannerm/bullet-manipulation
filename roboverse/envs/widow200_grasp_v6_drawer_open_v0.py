7e4b0e0162a342c0698129634b6822ae9d6d0e4c
Albert <albertyu@berkeley.edu>
2020-07-16 01:35:24 -0700

    shift drawer to right. Opening policy in main method works decently. TODO: debug drawer_grasping_only_policy/

--- roboverse/envs/widow200_grasp_v6_drawer_open_v0.py
+++ roboverse/envs/widow200_grasp_v6_drawer_open_v0.py
@@ -31,7 +31,7 @@ class Widow200GraspV6DrawerOpenV0Env(Widow200GraspV6BoxV0Env):
             camera_pitch=camera_pitch,
             **kwargs)
         self._env_name = "Widow200GraspV6DrawerOpenV0Env"
-        self._object_position_high = (.82, -.08, -.29)
+        self._object_position_high = (.82, -.07, -.29)
         self._object_position_low = self._object_position_high
         self._success_dist_threshold = success_dist_threshold
         # self._scaling_local_list = scaling_local_list
@@ -44,8 +44,8 @@ class Widow200GraspV6DrawerOpenV0Env(Widow200GraspV6BoxV0Env):
         self.open_only = open_only
 
         if not self.close_drawer_on_reset:
-            self._object_position_high = (.83, -.1, -.29)
-            self._object_position_low = (.83, -.1, -.29)
+            self._object_position_high = (.82, -.07, -.29)
+            self._object_position_low = (.82, -.07, -.29)
             self.scripted_traj_len = 25 # Give less time if drawer starts opened.
         if self.open_only:
             assert self.close_drawer_on_reset
@@ -90,7 +90,7 @@ class Widow200GraspV6DrawerOpenV0Env(Widow200GraspV6BoxV0Env):
         return np.array(handle_pos)
 
     def is_drawer_opened(self, widely=False):
-        opened_thresh = -0.01 if not widely else -0.08
+        opened_thresh = 0.0 if not widely else -0.05
         return self.get_drawer_bottom_pos()[1] < opened_thresh
 
     def get_info(self):
@@ -217,15 +217,193 @@ def drawer_open_policy(EPSILON, noise, margin, save_video, env):
         if save_video:
             utils.save_video('data/grasp_place_{}.avi'.format(i), images)
 
+def drawer_open_only_policy(EPSILON, noise, margin, save_video, env):
+    object_ind = 0
+    for i in range(50):
+        obs = env.reset()
+        print("obs", obs)
+        # object_pos[2] = -0.30
+
+        dist_thresh = 0.04 + np.random.normal(scale=0.01)
+        max_theta_action_magnitude = 0.2
+        grasp_target_theta = np.random.uniform(-np.pi / 2, np.pi / 2)
+        drawer_never_opened = True
+
+        images = [] # new video at the start of each trajectory.
+
+        for _ in range(env.scripted_traj_len):
+            state_obs = obs[env.fc_input_key]
+            obj_obs = obs[env.object_obs_key]
+            ee_pos = state_obs[:3]
+            object_pos = obj_obs[object_ind * 7 : object_ind * 7 + 3]
+            handle_offset = np.array([0, -0.01, 0])
+            handle_pos = env.get_handle_pos() + handle_offset
+            # Make robot aim a little to the left of the handle
+            ending_target_pos = np.array([0.73822169, -0.03909928, -0.25635483]) # Effective neutral pos.
+            object_lifted_with_margin = object_pos[2] > (
+                env._reward_height_thresh + margin)
+            # object_pos += np.random.normal(scale=0.02, size=(3,))
+
+            gripper_neutral_dist = np.linalg.norm(env._pos_init - ee_pos)
+            gripper_handle_dist = np.linalg.norm(handle_pos - ee_pos)
+            theta_action = 0.
+
+            if (gripper_handle_dist > dist_thresh
+                and not env.is_drawer_opened(widely=drawer_never_opened)):
+                print('approaching handle')
+                action = (handle_pos - ee_pos) * 7.0
+                xy_diff = np.linalg.norm(action[:2]/7.0)
+                if xy_diff > 0.75 * dist_thresh:
+                    action[2] = 0.5 # force upward action to avoid upper box
+                action = np.concatenate((action, np.asarray([theta_action,0.,0.])))
+            elif not env.is_drawer_opened(widely=drawer_never_opened):
+                print("opening drawer")
+                action = np.array([0, -1.0, 0])
+                # action = np.asarray([0., 0., 0.7])
+                action = np.concatenate(
+                    (action, np.asarray([0., 0., 0.])))
+            elif np.abs(ee_pos[2] - ending_target_pos[2]) > dist_thresh:
+                print("Lift upward")
+                print("np.abs(ee_pos[2] - ending_target_pos[2])", np.abs(ee_pos[2] - ending_target_pos[2]))
+                drawer_never_opened = False
+                action = np.array([0, 0, 0.7]) # force upward action to avoid upper box
+                action = np.concatenate(
+                    (action, np.asarray([theta_action, 0., 0.])))
+            else:
+                print("Move toward neutral")
+                action = (ending_target_pos - ee_pos) * 7.0
+                action = np.concatenate(
+                    (action, np.asarray([0., 0., 0.])))
+
+            noise_scalings = [noise] * 3 + [0.1 * noise] + [noise] * 2
+            action += np.random.normal(scale=noise_scalings)
+            action = np.clip(action, -1 + EPSILON, 1 - EPSILON)
+            # print(action)
+            obs, rew, done, info = env.step(action)
+            # print("rew", rew)
+
+            img = env.render_obs()
+            if save_video:
+                images.append(img)
+
+            time.sleep(0.05)
+
+        print('object pos: {}'.format(object_pos))
+        print('reward: {}'.format(rew))
+        print('--------------------')
+
+        if save_video:
+            utils.save_video('data/grasp_place_{}.avi'.format(i), images)
+
+# def drawer_grasping_only_policy(EPSILON, noise, margin, save_video, env):
+#     object_ind = 0
+#     margin = 0.025
+
+#     for i in range(50):
+#         obs = env.reset()
+
+#         dist_thresh = 0.045 + np.random.normal(scale=0.01)
+#         dist_thresh = np.clip(dist_thresh, 0.035, 0.060)
+
+#         for _ in range(env.scripted_traj_len):
+
+#             # if isinstance(observation, dict):
+#             #     object_pos = observation[env.object_obs_key][
+#             #                  object_ind * 7 : object_ind * 7 + 3]
+#             #     ee_pos = observation[env.fc_input_key][:3]
+#             # else:
+#             #     object_pos = observation[
+#             #                  object_ind * 7 + 8: object_ind * 7 + 8 + 3]
+#             #     ee_pos = observation[:3]
+#             state_obs = obs[env.fc_input_key]
+#             obj_obs = obs[env.object_obs_key]
+#             ee_pos = state_obs[:3]
+#             object_pos = obj_obs[object_ind * 7 : object_ind * 7 + 3]
+
+#             print("object_pos", object_pos)
+
+#             object_lifted_with_margin = object_pos[2] > (env._reward_height_thresh + margin)
+
+#             object_gripper_dist = np.linalg.norm(object_pos - ee_pos)
+#             theta_action = 0.
+
+#             # if object_gripper_dist > dist_thresh and env._gripper_open:
+#             #     print('approaching')
+#             #     print("dist_thresh", dist_thresh)
+#             #     action = (object_pos - ee_pos) * 7.0
+#             #     xy_diff = np.linalg.norm(action[:2] / 7.0)
+#             #     # if "Drawer" in env._env_name:
+#             #     #     pass
+#             #     #     # if xy_diff > dist_thresh:
+#             #     #     #     action[2] = 0.4 # force upward action to avoid upper box
+#             #     # else:
+#             #     if xy_diff > 0.02:
+#             #         action[2] = 0.0
+#             #     action = np.concatenate(
+#             #         (action, np.asarray([theta_action, 0., 0.])))
+#             # elif env._gripper_open:
+#             #     print('gripper closing')
+#             #     action = (object_pos - ee_pos) * 7.0
+#             #     action = np.concatenate(
+#             #         (action, np.asarray([0., -0.7, 0.])))
+#             # elif not object_lifted_with_margin:
+#             #     print('raise object upward')
+#             #     saction = np.asarray([0., 0., 0.7])
+#             #     action = np.concatenate(
+#             #         (action, np.asarray([0., 0., 0.])))
+#             # else:
+#             #     # Move above tray's xy-center.
+#             #     print("done")
+#             #     tray_info = roboverse.bullet.get_body_info(
+#             #         env._tray, quat_to_deg=False)
+#             #     tray_center = np.asarray(tray_info['pos'])
+#             #     action = (tray_center - ee_pos)[:2]
+#             #     action = np.concatenate(
+#             #         (action, np.asarray([0., 0., 0., 0.])))
+#             action = (object_pos - ee_pos)
+#             action = np.concatenate((action, np.array([0,0,0])))
+
+#             # action += np.random.normal(scale=noise, size=(6,))
+#             action[:3] += np.random.normal(scale=noise, size=(3,))
+#             action[3] += np.random.normal(scale=noise*0.1)
+#             action[4:] += np.random.normal(scale=noise, size=(2,))
+#             action = np.clip(action, -1 + EPSILON, 1 - EPSILON)
+#             print("action", action)
+
+#             next_observation, reward, done, info = env.step(action)
+
+#             if done:
+#                 break
+
 if __name__ == "__main__":
     EPSILON = 0.05
     noise = 0.2
     margin = 0.025
     save_video = True
 
-    env = roboverse.make("Widow200GraspV6DrawerGraspOnlyV0-v0",
-                         gui=True,
-                         reward_type='sparse',
-                         observation_mode='pixels_debug',
-                         noisily_open_drawer=True)
-    drawer_open_policy(EPSILON, noise, margin, save_video, env)
+    mode = "GraspOnly"
+
+    gui = True
+    reward_type = "sparse"
+    obs_mode = "pixels_debug"
+    if mode == "Open":
+        env = roboverse.make("Widow200GraspV6DrawerOpenV0-v0",
+                             gui=gui,
+                             reward_type=reward_type,
+                             observation_mode=obs_mode)
+        drawer_open_policy(EPSILON, noise, margin, save_video, env)
+    elif mode == "OpenOnly":
+        env = roboverse.make("Widow200GraspV6DrawerOpenOnlyV0-v0",
+                             gui=gui,
+                             reward_type=reward_type,
+                             observation_mode=obs_mode)
+        drawer_open_only_policy(EPSILON, noise, margin, save_video, env)
+    elif mode == "GraspOnly":
+        env = roboverse.make("Widow200GraspV6DrawerGraspOnlyV0-v0",
+                             gui=gui,
+                             reward_type=reward_type,
+                             observation_mode=obs_mode,
+                             noisily_open_drawer=True)
+        drawer_open_policy(EPSILON, noise, margin, save_video, env)
+    else:
+        raise NotImplementedError
