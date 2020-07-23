f89ad528d9ece6d4e102db4aba85f78cc9b47fec
Avi Singh <avisingh599@gmail.com>
2020-07-22 23:56:50 -0700

    Add max_iters to move to neutral action

--- roboverse/bullet/ik.py
+++ roboverse/bullet/ik.py
@@ -208,7 +208,10 @@ def move_to_neutral(RESET_JOINTS, robot_id):
     for i in range(len(RESET_JOINTS)):
         p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, RESET_JOINTS[i])
     joint_norm_dev_from_neutral = 1.0
-    while joint_norm_dev_from_neutral > 0.01:
+    max_iters = 100
+    iters = 0
+    while joint_norm_dev_from_neutral > 0.01 and iters < max_iters:
+        iters += 1
         p.stepSimulation()
         currJointStates = get_joint_positions(robot_id)[1][:len(RESET_JOINTS)]
         joint_norm_dev_from_neutral = np.linalg.norm(currJointStates - RESET_JOINTS)
