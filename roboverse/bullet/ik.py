060f7933dadaf3e0920604714aeb523dacd679aa
Albert Yu <albertyu@berkeley.edu>
2020-08-02 05:58:42 -0700

    Add move_to_neutral_slow to render neutral action more smoothly.

--- roboverse/bullet/ik.py
+++ roboverse/bullet/ik.py
@@ -216,6 +216,24 @@ def move_to_neutral(RESET_JOINTS, robot_id):
         currJointStates = get_joint_positions(robot_id)[1][:len(RESET_JOINTS)]
         joint_norm_dev_from_neutral = np.linalg.norm(currJointStates - RESET_JOINTS)
 
+def move_to_neutral_slow(RESET_JOINTS, robot_id, image_size, view_matrix, projection_matrix):
+    images = []
+    for i in range(len(RESET_JOINTS)):
+        p.setJointMotorControl2(robot_id, i, p.POSITION_CONTROL, RESET_JOINTS[i])
+    joint_norm_dev_from_neutral = 1.0
+    max_iters = 100
+    max_images = 4
+    iters = 0
+    while joint_norm_dev_from_neutral > 0.01 and iters < max_iters:
+        iters += 1
+        p.stepSimulation()
+        img, _, _ = bullet.render(image_size, image_size,
+                    view_matrix, projection_matrix)
+        images.append(img)
+        currJointStates = get_joint_positions(robot_id)[1][:len(RESET_JOINTS)]
+        joint_norm_dev_from_neutral = np.linalg.norm(currJointStates - RESET_JOINTS)
+    return images[:max_images]
+
 #################
 ####  drawer  ###
 #################
