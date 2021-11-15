e78f0ee71a0ae264490c1b0fcae2c1ad3d161eef
Ilya Kostrikov <ikostrikov@gmail.com>
2021-11-14 16:33:55 -0800

    Update data generation.

--- training/generate_data.py
+++ training/generate_data.py
@@ -5,62 +5,74 @@ import time
 import numpy as np
 import roboverse as rv
 from gym.wrappers import TimeLimit
+from moviepy.editor import ImageSequenceClip
+from tqdm import tqdm
 
 from training.chunk_writer import ChunkWriter
 
-#change to done 2 seperate
-#spacemouse = rv.devices.SpaceMouse(DoF=6)
-env = rv.make('RemoveLid-v0', gui=False)
-#env = rv.make('RemoveLid-v0', gui=False)
-# env = rv.make('MugDishRack-v0', gui=False)
-# env = rv.make('FlipPot-v0', gui=True)
 
-time_limit = 50
-env = TimeLimit(env, max_episode_steps=time_limit)
+def main():
+    #change to done 2 seperate
+    #spacemouse = rv.devices.SpaceMouse(DoF=6)
+    env = rv.make('RemoveLid-v0', gui=False)
+    #env = rv.make('RemoveLid-v0', gui=False)
+    # env = rv.make('MugDishRack-v0', gui=False)
+    # env = rv.make('FlipPot-v0', gui=True)
 
-data_folder = 'dataset'
-if os.path.exists(data_folder):
-    shutil.rmtree(data_folder)
-os.makedirs(data_folder, exist_ok=True)
+    time_limit = 50
+    env = TimeLimit(env, max_episode_steps=time_limit)
 
-num_traj = 10
-chunk_writter = ChunkWriter(data_folder, (time_limit + 1) * num_traj)
+    num_traj = 1
+    data_folder = os.path.join('dataset', f'traj_{num_traj}')
+    if os.path.exists(data_folder):
+        shutil.rmtree(data_folder)
+    os.makedirs(data_folder, exist_ok=True)
 
-start = time.time()
-for j in range(num_traj):
-    env.reset()
-    env.demo_reset()
-    done = False
-    returns = 0
-    while not done:
+    chunk_writter = ChunkWriter(data_folder, (time_limit + 1) * num_traj)
+
+    start = time.time()
+    images = []
+    for j in range(num_traj):
+        env.reset()
+        env.demo_reset()
+        done = False
+        returns = 0
+        pbar = tqdm(total=time_limit)
+        while not done:
+            pbar.update(1)
+            img = env.render_obs()
+            images.append(img)
+            # human_action = spacemouse.get_action()
+            action, noisy_action = env.get_demo_action()
+            # noisy_action = env.action_space.sample()
+
+            next_observation, reward, done, info = env.step(noisy_action)
+            if not done or 'TimeLimit.truncated' in info:
+                mask = 1.0
+            else:
+                mask = 0.0
+
+            chunk_writter.add_transiton(img, action, reward, mask, done)
+
+            returns += reward
+
         img = env.render_obs()
-        # human_action = spacemouse.get_action()
-        action, noisy_action = env.get_demo_action()
-        # noisy_action = env.action_space.sample()
+        chunk_writter.add_transiton(img,
+                                    np.zeros_like(action),
+                                    0,
+                                    0,
+                                    False,
+                                    dummy=True)
 
-        next_observation, reward, done, info = env.step(noisy_action)
-        if not done or 'TimeLimit.truncated' in info:
-            mask = 1.0
-        else:
-            mask = 0.0
+        print(f"Episode {j} returns {returns}")
 
-        chunk_writter.add_transiton(img, action, reward, mask, done)
+    print('Simulation Time:', (time.time() - start) / num_traj)
 
-        returns += reward
+    path = './rollout.mp4'
 
-    img = env.render_obs()
-    chunk_writter.add_transiton(img,
-                                np.zeros_like(action),
-                                0,
-                                0,
-                                False,
-                                dummy=True)
+    video = ImageSequenceClip(images, fps=24)
+    video.write_videofile(path)
 
-    print(f"Episode {j} returns {returns}")
 
-print('Simulation Time:', (time.time() - start) / num_traj)
-
-path = './rollout.mp4'
-
-# video = ImageSequenceClip(images, fps=24)
-# video.write_videofile(path)
+if __name__ == "__main__":
+    main()
