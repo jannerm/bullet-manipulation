33aa3e65bc13de4662421d89b22d012e22001ac1
Patrick Yin <patrickhaoy@berkeley.edu>
2022-01-17 23:15:22 -0800

    v0 swap task

--- shapenet_scripts/visualize_env_test.py
+++ shapenet_scripts/visualize_env_test.py
@@ -1,6 +1,22 @@
 import roboverse as rv
 
-env = rv.make("SawyerRigAffordances-v1", gui=True, expl=True, reset_interval=1)#, env_type='tray')
+env = rv.make(
+    "SawyerRigAffordances-v0", 
+    gui=True, 
+    expl = False,
+    random_color_p=1,
+    max_episode_steps = 150,
+    obs_img_dim = 64,
+    claw_spawn_mode='fixed', #Maybe Uniform Instead, so distractor is more distracting....?
+    #drawer_yaw_setting = (0, 360),
+    demo_action_variance = 0.1,
+    color_range = (0, 255//2),
+    #max_distractors = 4,
+    #reset_interval=1,
+    test_env=True,
+    env_type='top_drawer',
+    swap_eval_task=True,
+)
 ts = 150
 
 for i in range(100):
