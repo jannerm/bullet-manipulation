cc9103e1a1d6946afeb4012a246fb47a341d67d7
patrickhaoy <patrickhaoy@berkeley.edu>
2022-11-02 21:51:12 -0700

    add val env

---
+++ shapenet_scripts/diverse_val_demo_collector.py
@@ -0,0 +1,130 @@
+import roboverse
+import numpy as np
+import pickle as pkl
+from tqdm import tqdm
+from roboverse.utils.renderer import EnvRenderer, InsertImageEnv
+from roboverse.bullet.misc import quat_to_deg
+import os
+from PIL import Image
+import math
+import argparse
+from multiprocess import Pool
+import gc
+
+NUM_TIMESTEPS=100
+
+def collect(id):
+    state_env = roboverse.make(
+        'SawyerDiverseVAL-v0', 
+        expl=True, 
+        **kwargs
+    )
+
+    # FOR TESTING, TURN COLORS OFF
+    imsize = state_env.obs_img_dim
+
+    renderer_kwargs=dict(
+            create_image_format='HWC',
+            output_image_format='CWH',
+            width=imsize,
+            height=imsize,
+            flatten_image=True,)
+
+    renderer = EnvRenderer(init_camera=None, **renderer_kwargs)
+    env = InsertImageEnv(state_env, renderer=renderer)
+    imlength = env.obs_img_dim * env.obs_img_dim * 3
+
+    success = 0
+    returns = 0
+    act_dim = env.action_space.shape[0]
+    num_datasets = 0
+    demo_dataset = []
+
+    recon_dataset = {
+        'observations': np.zeros((args.num_trajectories_per_demo, NUM_TIMESTEPS, imlength), dtype=np.uint8),
+        'env': np.zeros((args.num_trajectories_per_demo, imlength), dtype=np.uint8),
+        'skill_id': np.zeros((args.num_trajectories_per_demo, ), dtype=np.uint8)
+    }
+
+    for j in tqdm(range(args.num_trajectories_per_demo)):
+        # is_done = False
+        # while not is_done:
+        env.demo_reset()
+        recon_dataset['env'][j, :] = np.uint8(env.render_obs().transpose()).flatten()
+        trajectory = {
+            'observations': [],
+            'next_observations': [],
+            'actions': np.zeros((NUM_TIMESTEPS, act_dim), dtype=np.float),
+            'rewards': np.zeros((NUM_TIMESTEPS), dtype=np.float),
+            'terminals': np.zeros((NUM_TIMESTEPS), dtype=np.uint8),
+            'agent_infos': np.zeros((NUM_TIMESTEPS), dtype=np.uint8),
+            'env_infos': np.zeros((NUM_TIMESTEPS), dtype=np.uint8),
+            'skill_id': -1,
+        }
+        for i in range(NUM_TIMESTEPS):
+            img = np.uint8(env.render_obs())
+            recon_dataset['observations'][j, i, :] = img.transpose().flatten()
+
+            observation = env.get_observation()
+
+            action, done = env.get_demo_action(first_timestep=(i == 0), return_done=True)
+            next_observation, reward, _, info = env.step(action)
+            # if done:
+            #     is_done = True
+
+            trajectory['observations'].append(observation)
+            trajectory['actions'][i, :] = action
+            trajectory['next_observations'].append(next_observation)
+            trajectory['rewards'][i] = reward
+            trajectory['skill_id'] = -1
+            recon_dataset['skill_id'][j] = -1
+
+        demo_dataset.append(trajectory)
+
+    ## Save contents    
+    file = open(os.path.join(prefix, f'{id}_val_demos.pkl'), 'wb')
+    pkl.dump(demo_dataset, file)
+    file.close()
+    del demo_dataset
+    gc.collect()
+    demo_dataset = []
+
+    np.save(os.path.join(prefix, f'{id}_val_images.npy'), recon_dataset)
+    del recon_dataset
+    gc.collect()
+    recon_dataset = []
+
+    env.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--save_path", type=str, default='/media/ashvin/data1/patrickhaoy/data/test')
+    parser.add_argument("--num_trajectories", type=int, default=1)
+    parser.add_argument("--num_trajectories_per_demo", type=int, default=1)
+    parser.add_argument("--num_threads", type=int, default=1)
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--fix_camera_yaw_pitch', action='store_true')
+
+    args = parser.parse_args()
+    assert args.num_trajectories % args.num_trajectories_per_demo == 0
+    prefix = args.save_path
+
+    if not os.path.exists(prefix):
+        os.makedirs(prefix)
+
+    kwargs = {
+        'reset_interval': 1,
+        'expert_policy_std': .05,
+        'downsample': True,
+        'env_obs_img_dim': 196,
+        'random_init_gripper_pos': True,
+        'random_init_gripper_yaw': False,
+        'use_target_config': args.debug,
+        'fix_camera_yaw_pitch': args.fix_camera_yaw_pitch,
+    }
+
+    pool = Pool(args.num_threads)
+    ids = [id for id in range(args.num_trajectories // args.num_trajectories_per_demo)]
+    results = pool.map(collect, ids)
+    pool.close()
+    pool.join()
---
+++ shapenet_scripts/visualize_env_val.py
@@ -0,0 +1,94 @@
+import roboverse as rv
+import numpy as np
+import skvideo.io
+
+# from experiments.kuanfang.iql.drawer_pnp_commands import drawer_pnp_commands
+# from experiments.kuanfang.iql.drawer_pnp_single_obj_commands import drawer_pnp_single_obj_commands
+from rlkit.experimental.kuanfang.envs.drawer_pnp_push_commands import drawer_pnp_push_commands
+from roboverse.envs.configs.drawer_pnp_push_env_configs import drawer_pnp_push_env_configs
+
+ts = 100
+num_traj = 75
+
+#obs_img_dim=196, 
+env = rv.make(
+    "SawyerDiverseVAL-v0", #"SawyerDiverseDrawerPnpPush-v0", #"SawyerRigAffordances-v6", #"SawyerResetFreeDrawerPnpPush-v0", 
+    gui=False, 
+    expl=True, 
+    reset_interval=1, #10, 
+    #reset_gripper_interval=1,
+    env_obs_img_dim=196, 
+    # test_env=True, 
+    # test_env_command=drawer_pnp_push_commands[14],
+    # use_test_env_command_sequence=False,
+    # demo_num_ts=ts,
+    # fixed_drawer_yaw=171.86987153482346,
+    # fixed_drawer_quadrant=1,
+    expert_policy_std=.05,
+    downsample=False,
+    fix_camera_yaw_pitch=True,
+    #configs=drawer_pnp_push_env_configs[1],
+    # fixed_task='move_obj_slide',
+    # random_init_gripper_pos=True
+)
+
+save_video = True
+
+if save_video:
+    video_save_path = '/media/ashvin/data1/patrickhaoy/data/test/'
+    num_traj = 2
+    observations = np.zeros((num_traj*ts, 196, 196, 3))
+
+
+tasks_success = dict()
+tasks_count = dict()
+for i in range(num_traj):
+    print(i)
+    env.demo_reset()
+    curr_task = env.curr_task
+    is_done = False
+    for t in range(ts):
+        # np.uint8(env.render_obs())
+        if save_video:
+            img = np.uint8(env.render_obs())
+            # from PIL import Image
+            # im = Image.fromarray(img)
+            # im.save(video_save_path + "debug.jpeg")
+            # exit()
+            observations[i*ts + t, :] = img
+        action = env.get_demo_action(first_timestep=(t == 0))
+        # sign = 1 if i % 2 == 0 else -1
+        # action = np.array([0, -1, 0, 0, -1])
+        done = False
+        next_observation, reward, _, info = env.step(action)
+        if done and not is_done:
+            is_done = True 
+            
+            if curr_task not in tasks_success.keys():
+                tasks_success[curr_task] = 1
+            else:
+                tasks_success[curr_task] += 1
+            print("success")
+    
+    if curr_task not in tasks_count.keys():
+        tasks_count[curr_task] = 1
+    else:
+        tasks_count[curr_task] += 1
+
+print()
+total_successes = 0
+
+for task in tasks_count.keys():
+    num_successes = tasks_success.get(task, 0)
+    num_tries = tasks_count.get(task, 0)
+    print(f"{task} | success rate: {num_successes/num_tries}, count: {num_tries} \n")
+    total_successes += num_successes
+
+print(f"Overall success rate: {total_successes/num_traj}, count: {num_traj} \n")
+
+
+if save_video:
+    writer = skvideo.io.FFmpegWriter(video_save_path + "debug.mp4")
+    for i in range(num_traj*ts):
+        writer.writeFrame(observations[i, :, :, :])
+    writer.close()
